4d8f2bb4556a7ec7c44709e66ec2978cbb303cb0
AThusithaGayan <thusithag@aplicy.com>
2026-05-04 21:20:11 +0530

    Add CORS middleware and implement text cleaning function for report generation

--- controllers/report_controller.py
+++ controllers/report_controller.py
@@ -1,4 +1,5 @@
 import os
+import re
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
@@ -8,13 +9,41 @@ from services.report import generate_report
 
 router = APIRouter()
 
+
+
+
+
+def clean_text(text: str) -> str:
+    # 1. Convert all line breaks + tabs to space
+    text = re.sub(r'[\n\r\t]+', ' ', text)
+
+    # 2. Remove multiple spaces
+    text = re.sub(r'\s+', ' ', text)
+
+    # 3. Remove weird control characters (important for PDF crashes)
+    text = re.sub(r'[^\x20-\x7E\u00A0-\uFFFF]', '', text)
+
+    # 4. Trim
+    text = text.strip()
+    print("Cleaned Text:", text)
+    return text
+
+
+
+
+
+
 @router.post("/generate-report")
 def generate_report_api(data: TextInput):
+    print("Received Text:", data.text)
     try:
-        # Step 1: Extract structured data
-        extracted_data = extract_json(data)
-        print("Extracted Data:", extracted_data)
 
+        cleaned_text = clean_text(data)
+        print("Cleaned Text:", cleaned_text)
+        # Step 1: Extract structured data
+        extracted_data = extract_json(cleaned_text)
+        print("Extracted Data:", extracted_data)
+            
         # Step 2: Generate PDF
         filepath = generate_report(extracted_data)
 
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from controllers.report_controller import router as report_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+
+origins = [
+    "https://api.usoapnote.com",       
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(report_router)
